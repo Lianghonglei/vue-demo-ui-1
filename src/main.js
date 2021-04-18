db8a9f834354a823607006898b826eceaa99fb21
lei <895641390@qq.com>
2021-04-18 13:55:59 +0800

    need install zhuangzhuang-ui

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {Button, ButtonGroup, Icon} from 'zhuangzhuang-ui-test-1'
+
+window.console.log(Button)
+window.console.log(ButtonGroup)
+window.console.log(Icon)
 
 Vue.config.productionTip = false
 
